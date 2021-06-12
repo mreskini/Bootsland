d95c96b948d205c50d9d01c1d4fe65b09c4599de
Neo.Eskini <mreskini30@gmail.com>
2021-06-12 14:10:41 +0430

    added the hero section empty file to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Hero from "./components/hero";
+
 function App() {
     return (
       <>
-        Hello world neo is here
+        <Hero />
       </>
     );
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,5 @@
+export default function Hero(){
+    return (
+        <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, molestiae!</div>
+    )
+}
