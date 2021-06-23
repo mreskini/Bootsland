6c0204d711d1f935cc4eebceda993886de50473c
Neo.Eskini <mreskini30@gmail.com>
2021-06-23 16:14:46 +0430

    changed the header padding and margins a little bit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
 
 export default function  Header(){
     return(
-        <div className="container-fluid mt-4">
+        <div className="container-fluid mt-1">
             <div className="col-11 mx-auto">
                 <Navbar variant="light" expand="lg" sticky="top">
                     <Navbar.Brand href="#home">
@@ -13,17 +13,17 @@ export default function  Header(){
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link className="h5 my-auto">Home</Nav.Link>
-                            <Nav.Link className="h5 my-auto">Pages</Nav.Link>
-                            <Nav.Link className="h5 my-auto">Shop</Nav.Link>
-                                <NavDropdown title="Features" className="h5 my-auto">
-                                    <NavDropdown.Item className="h5 my-auto">Blog</NavDropdown.Item>
-                                    <NavDropdown.Item className="h5 my-auto">Counter</NavDropdown.Item>
-                                    <NavDropdown.Item className="h5 my-auto">Team</NavDropdown.Item>
+                        <Nav className="ml-auto mr-5">
+                            <Nav.Link className="h6 my-auto bold-600">Home</Nav.Link>
+                            <Nav.Link className="h6 my-auto bold-600">Pages</Nav.Link>
+                            <Nav.Link className="h6 my-auto bold-600">Shop</Nav.Link>
+                                <NavDropdown title="Features" className="h6 my-auto bold-600">
+                                    <NavDropdown.Item className="h6 my-auto">Blog</NavDropdown.Item>
+                                    <NavDropdown.Item className="h6 my-auto">Counter</NavDropdown.Item>
+                                    <NavDropdown.Item className="h6 my-auto">Team</NavDropdown.Item>
                                 </NavDropdown>
                         </Nav>
-                        <Button className="btn btn-primary-color h5 my-auto ml-auto">Buy Now</Button>
+                        <Button className="btn btn-primary-color h5 my-auto">Buy Now</Button>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
